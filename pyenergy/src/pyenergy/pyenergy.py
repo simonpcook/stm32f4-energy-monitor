326fcf5a7d8986c1afa9acc2078fcd013c4097f7
Simon Cook <simon.cook@embecosm.com>
2014-07-11 11:58:02 +0100

    Add fixes from James visiting

--- pyenergy/src/pyenergy/pyenergy.py
+++ pyenergy/src/pyenergy/pyenergy.py
@@ -294,7 +294,7 @@ class EnergyMonitor(object):
 
         runs = unpack("=L", b)
         debug("Runs: {0}".format(runs[0]))
-        return bool(runs[0])
+        return int(runs[0])
 
     # Reset the number of runs counts to 0
     def clearNumberOfRuns(self, m_point=1):
@@ -308,11 +308,11 @@ class EnergyMonitor(object):
             number of runs if we are waiting for a trigger.
         """
 
-        runs = self.getNumberOfRuns()
+        runs = self.getNumberOfRuns(m_point)
         if runs > 1:
             warning("More than one measurement has completed (expected one)")
         if not self.isRunning(m_point) and runs > 0:
-            self.clearNumberOfRuns()
+            self.clearNumberOfRuns(m_point)
             return True
         return False
 
